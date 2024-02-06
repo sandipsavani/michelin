3b7bac82e94f9b5e32d4bdb1dd771c646a0f2dab
Ajay Bokde <ajay_bokde@persistent.com>
2024-02-06 23:17:09 +0530

    adding promotion details

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -2,3 +2,11 @@
 $(document).ready(function() {
   $('#dataTable').DataTable();
 });
+
+$(document).ready(function() {
+  $('#dataTable2').DataTable();
+});
+
+$('#dataTable2').DataTable({
+  "ordering": false
+});
